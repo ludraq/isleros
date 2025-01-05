a8981718f4663fe1ad6140fcb101a3d348dcd742
ludraq <161099492+ludraq@users.noreply.github.com>
2025-01-05 16:41:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -123,6 +123,9 @@ function calcularTotales() {
   const diferencia = total - total_general;
   document.getElementById('diferencia').value = diferencia.toFixed(2);
   document.getElementById('diferencia_display').textContent = formatCOP(Math.floor(diferencia));
+
+  const precioCombustibleInput = document.getElementById('precio_combustible'); // Asegúrate de que este ID sea el correcto
+  const precioCombustible = parseFloat(precioCombustibleInput.value) || 0;
   if (diferencia === 0 && precioCombustible > 0) {
     alert('¡La planilla quedó cuadrada!');
   }
